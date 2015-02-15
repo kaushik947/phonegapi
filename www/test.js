639d005c37c1097b24652550caac59a00d3539d5
kaushik947 <kaushik7ronaldo@gmail.com>
2015-02-15 18:42:22 +0530

    Update test.js

--- www/test.js
+++ www/test.js
@@ -30,7 +30,7 @@ function getTokens() {
  * Replace with your own!
  */
 var params = {
-    client_id: '348679480640-lrk25uau8sih9b6k8o7h49adk5va8pdm.apps.googleusercontent.com',
+    client_id: '651010955617-i5nog5noq43b52l7d34na4e90ih0nj1o.apps.googleusercontent.com',
     client_secret: 'iEDFnDYQ300cNXi1cEOxVfMy',
     scope: 'https://www.googleapis.com/auth/games',
     callback: function(error, tokens) {
